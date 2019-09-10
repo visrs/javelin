99bf451e84e24e757c2c80290b023329e134cbb2
Patrick Auernig <patrick.auernig@mykolab.com>
2019-09-10 23:06:55 +0200

    Implement std error trait for codec errors

--- javelin-codec/src/error.rs
+++ javelin-codec/src/error.rs
@@ -1,4 +1,4 @@
-use std::{io, result};
+use std::{io, result, error, fmt};
 
 
 pub type Result<T> = result::Result<T, Error>;
@@ -13,6 +13,24 @@ pub enum Error {
     DecoderConfigurationRecordMissing,
     AudioSpecificConfigurationMissing,
     UnsupportedConfigurationRecordVersion(u8),
+}
+
+impl error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut w = |s: &str| { write!(f, "{}", s) };
+
+        match self {
+            Error::IoError(inner) => w(&inner.to_string()),
+            Error::Custom(inner) => w(inner),
+            Error::ParseError(inner) => w(inner),
+            Error::NotEnoughData => w("Not enough data"),
+            Error::DecoderConfigurationRecordMissing => w("Decoder configuration record missing"),
+            Error::AudioSpecificConfigurationMissing => w("Audio specific configuration missing"),
+            Error::UnsupportedConfigurationRecordVersion(ver) => w(&format!("Unsupported configuration record version {}", ver)),
+        }
+    }
 }
 
 impl From<io::Error> for Error {
